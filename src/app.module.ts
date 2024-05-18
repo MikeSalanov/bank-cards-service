b3f4b6c154a865673c75c7f5ce7e24c30884c383
Michael <michael@simpleswap.io>
2024-05-18 03:43:49 +0300

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+<<<<<<< HEAD
 
 @Module({
   imports: [],
   controllers: [],
   providers: [],
+=======
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+>>>>>>> 490ec6d (add project bank cards)
 })
 export class AppModule {}
